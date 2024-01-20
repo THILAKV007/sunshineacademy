70026515f81916156bcc3911b7d7d8003e1087b1
Taha <taha.iu.bd.com>
2024-01-20 23:48:14 +0600

    footer responsive and contact us

--- src/pages/Contact_us/contact_us.js
+++ src/pages/Contact_us/contact_us.js
@@ -1,40 +1,43 @@
-import { forwardRef } from "react";
-import "../Contact_us/contact_us.css";
-const ContactUs=forwardRef((props,ref)=> {
+import { forwardRef } from 'react'
+import '../Contact_us/contact_us.css'
+const ContactUs = forwardRef((props, ref) => {
   return (
-    <section className="about-us-bg" ref={ref}> 
-      <div className="heading-text-style">Contact Us</div>
-      <div className="sub-heading-style">
+    <section className='about-us-bg' ref={ref}>
+      <div className='heading-text-style'>Contact Us</div>
+      <div className='sub-heading-style'>
         We offered industries leading course for medical coding & Medical
         billing course for medical coding & Medical billing at
       </div>
-      <div className="contact-content">
-        <img src="/assets/contact_us_bg.png" height={630}></img>
-        <div className="contact-form">
-          <form>
-            <label className="form-title-style">First Name</label>
-          </form>
-          <input type="text" name="name"  />
-          <form style={{ marginTop: 50 }}>
-            <label className="form-title-style">Mail-id</label>
-          </form>
-          <input type="text" name="name"  />
-          <form style={{ marginTop: 50 }}>
-            <label className="form-title-style">Phone Number</label>
-          </form>
-          <input type="text" name="name"  />
-          <form style={{ marginTop: 50 }}>
-            <label className="form-title-style">Message</label>
-          </form>
-          <input type="text" name="name"  />
-          <div className="contact-us-btn">
-        Contact Us
-     </div>
+      <div className='contact-content'>
+        <div style={{ width: '100%' }}>
+          {' '}
+          <img
+            src='/assets/contact_us_bg.png'
+            style={{ maxWidth: '100%', height: 'auto' }}
+            alt=''
+          />
         </div>
-        
+        <div className='contact-form'>
+          <form>
+            <label className='form-title-style'>First Name</label>
+          </form>
+          <input type='text' name='name' />
+          <form style={{ marginTop: 50 }}>
+            <label className='form-title-style'>Mail-id</label>
+          </form>
+          <input type='text' name='name' />
+          <form style={{ marginTop: 50 }}>
+            <label className='form-title-style'>Phone Number</label>
+          </form>
+          <input type='text' name='name' />
+          <form style={{ marginTop: 50 }}>
+            <label className='form-title-style'>Message</label>
+          </form>
+          <input type='text' name='name' />
+          <div className='contact-us-btn'>Contact Us</div>
+        </div>
       </div>
-     
     </section>
-  );
-});
-export default ContactUs;
+  )
+})
+export default ContactUs
--- src/pages/footer/footer.js
+++ src/pages/footer/footer.js
@@ -1,27 +1,159 @@
-import "../footer/footer.css";
+import Box from '@mui/material/Box'
+
 function Footer() {
   return (
-    <section className="footer-bg">
-      <div className="footer-contents">
-        <img src="/assets/footer-logo.png"></img>
-        <div>
-          <div className="main-heading-style">Quick Links</div>
-          <div className="quick-sub-heading-style">Course</div>
-          <div className="quick-sub-heading-style">Training</div>
-          <div className="quick-sub-heading-style">About Us</div>
-          <div className="quick-sub-heading-style">Blog</div>
-          <div className="quick-sub-heading-style">Contact Us</div>
-        </div>
-        <div>
-          <div className="main-heading-style">Follow Us On</div>
-          <div className="follow-links">
-            <img src="/assets/Facebook.png"></img>
-            <img src="/assets/Twitter.png"></img>
-          </div>
-          <div className="terms-policy">Terms and condition</div>
-        </div>
-      </div>
-    </section>
-  );
+    <Box
+      sx={{
+        backgroundColor: '#f43e04',
+        height: 'auto',
+        width: 'auto',
+      }}
+      className='footer-bg'
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          gap:2,
+          flexDirection: { xs: 'column', md: 'row' },
+          justifyContent: 'space-between',
+          paddingTop: '18px',
+          marginLeft: '4%',
+          marginRight: '5%',
+          alignItems: 'center',
+        }}
+        className='footer-contents'
+      >
+        <img src='/assets/footer-logo.png' alt='Footer Logo' />
+        <Box>
+          <Box
+            sx={{
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '32px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+            }}
+            className='main-heading-style'
+          >
+            Quick Links
+          </Box>
+          <Box
+            sx={{
+              marginTop: '8px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 300,
+              lineHeight: 'normal',
+            }}
+            className='quick-sub-heading-style'
+          >
+            Course
+          </Box>
+          <Box
+            sx={{
+              marginTop: '8px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 300,
+              lineHeight: 'normal',
+            }}
+            className='quick-sub-heading-style'
+          >
+            Training
+          </Box>
+          <Box
+            sx={{
+              marginTop: '8px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 300,
+              lineHeight: 'normal',
+            }}
+            className='quick-sub-heading-style'
+          >
+            About Us
+          </Box>
+          <Box
+            sx={{
+              marginTop: '8px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 300,
+              lineHeight: 'normal',
+            }}
+            className='quick-sub-heading-style'
+          >
+            Blog
+          </Box>
+          <Box
+            sx={{
+              marginTop: '8px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 300,
+              lineHeight: 'normal',
+            }}
+            className='quick-sub-heading-style'
+          >
+            Contact Us
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            sx={{
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '32px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+            }}
+            className='main-heading-style'
+          >
+            Follow Us On
+          </Box>
+          <Box
+            sx={{
+              marginTop: '16px',
+              display: 'flex',
+              gap: '14px',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            className='follow-links'
+          >
+            <img src='/assets/Facebook.png' alt='Facebook' />
+            <img src='/assets/Twitter.png' alt='Twitter' />
+          </Box>
+          <Box
+            sx={{
+              marginTop: '18px',
+              color: '#000',
+              fontFamily: 'Mitr',
+              fontSize: '20px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+            }}
+            className='terms-policy'
+          >
+            Terms and condition
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
-export default Footer;
+
+export default Footer
