b51b1b620ecc68afeb4144f9df6cf6030c79d2a1
Najmul Hassan Sunny <taha.iu.bd@gmail.com>
2024-12-11 23:02:07 +0800

    reduced font size

--- src/pages/specilization/our_specialization.js
+++ src/pages/specilization/our_specialization.js
@@ -1,62 +1,72 @@
-import "../specilization/specialization.css";
+import '../specilization/specialization.css'
 
 function OurSpecialization() {
   return (
-    <section className="about-us-bg">
-      <div className="heading-text-style">Our Specialization</div>
-      <div className="specialization-types">
-        <div className="types-bg">
-          <span className="set-bg-circle">
-            <img src="/assets/medical-coding.png" style={{ padding: 16 }}></img>
+    <section className='about-us-bg'>
+      <div className='heading-text-style'>Our Specialization</div>
+      <div className='specialization-types'>
+        <div className='types-bg'>
+          <span className='set-bg-circle'>
+            <img src='/assets/medical-coding.png' style={{ padding: 16 }}></img>
           </span>
-          <span className="specialization-types-style">Medical coding</span>
+          <span className='specialization-types-style'>Medical coding</span>
         </div>
-        <div className="types-bg">
-          <span className="set-bg-circle">
+        <div className='types-bg'>
+          <span className='set-bg-circle'>
             <img
-              src="/assets/medical-billing.png"
+              src='/assets/medical-billing.png'
               style={{ padding: 16 }}
             ></img>
           </span>
-          <span className="specialization-types-style">Medical Billing</span>
+          <span className='specialization-types-style'>Medical Billing</span>
         </div>
-        <div className="types-bg">
-          <span className="set-bg-circle">
+        <div className='types-bg'>
+          <span className='set-bg-circle'>
             <img
-              src="/assets/health-insurance.png"
+              src='/assets/health-insurance.png'
               style={{ padding: 16 }}
             ></img>
           </span>
-          <span className="specialization-types-style">
+          <span className='specialization-types-style'>
             Healthcare Training
           </span>
         </div>
       </div>
-      <div className="explanation">
+      <div className='explanation'>
         <ul>
-          <li className="explanation-style">
+          <li className='explanation-style'>
             Faculties are having 20+ years of real-time coding experience in
             multi-specialty coding with multiple certification
           </li>
-          <li className="explanation-style">
+          <li className='explanation-style'>
             Individual attention to all the students
           </li>
-          <li className="explanation-style">
+          <li className='explanation-style'>
             All trained students shall be placed in reputed companies
           </li>
-          <li className="explanation-style">
+          <li className='explanation-style'>
             Not only we will teach you the skills and knowledge, but we will
             also provide you tips and guidance for achieving your career goals
           </li>
         </ul>
       </div>
-      <div style={{color:"#6C6565",fontSize:34,fontWeight:300,textAlign:"center",marginTop:105}}>
-      All of us at SSA (Sun Shine Academy), look forward to helping you get the career training you need to get ahead in life.<br/><br/>Enroll today – and take the next step toward a brighter tomorrow.
-     </div>
-     <div className="enroll-btn">
-        Enroll Now
-     </div>
+      <div
+        style={{
+          color: '#6C6565',
+          fontSize: 24,
+          fontWeight: 300,
+          textAlign: 'center',
+          marginTop: 105,
+        }}
+      >
+        All of us at SSA (Sun Shine Academy), look forward to helping you get
+        the career training you need to get ahead in life.
+        <br />
+        <br />
+        Enroll today – and take the next step toward a brighter tomorrow.
+      </div>
+      <div className='enroll-btn'>Enroll Now</div>
     </section>
-  );
+  )
 }
-export default OurSpecialization;
+export default OurSpecialization
